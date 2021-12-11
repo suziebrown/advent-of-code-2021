9ffc6f386f1a312778c5ef015571a9cca1720062
Suzie Brown <suzie.brown42@gmail.com>
2021-12-11 12:11:03 +0000

    Day 8 complete

--- day08.py
+++ day08.py
@@ -15,6 +15,22 @@ PLAIN_TEXT_ENCODINGS = {
 
 CIPHER_TEXT_LETTERS = ["a", "b", "c", "d", "e", "f", "g"]
 PLAIN_TEXT_LETTERS = ["A", "B", "C", "D", "E", "F", "G"]
+NUMBER_OF_TEST_DIGITS = 4
+
+
+class Test_data:
+    def __init__(self, input_list: List[str]):
+        self.coded_list = input_list
+        self.decoded_digits = []
+        self.sort_each_string()
+
+    def decode(self, code: dict[str, int]):
+        self.decoded_digits = [code[string] for string in self.coded_list]
+
+    def sort_each_string(self):
+        for (index, string) in enumerate(self.coded_list):
+            self.coded_list[index] = "".join(sorted(string))
+        
 
 class Training_data:
     def __init__(self, input_list: List[str]):
@@ -28,8 +44,10 @@ class Training_data:
     def decode(self):
         self.decipher()
         deciphered_list = self.get_deciphered_input()
-        # TODO: actually want to fill in the rest of self.code (probably clear it first then loop through all)
         decoded_list = [PLAIN_TEXT_ENCODINGS[string] for string in deciphered_list]
+        self.code.clear()
+        for digit in range(0, 10):
+            self.code.update({self.coded_list[digit]: decoded_list[digit]})
 
     def get_deciphered_input(self) -> List[str]:
         deciphered_list = []
@@ -39,8 +57,7 @@ class Training_data:
                 deciphered_string = deciphered_string.replace(self.cipher[letter], letter)
             deciphered_string = "".join(sorted(deciphered_string))
             deciphered_list.append(deciphered_string)
-        return deciphered_list
-                
+        return deciphered_list          
 
     def decipher(self):
         self.decode_1478()
@@ -114,34 +131,55 @@ class Training_data:
 
 
 def main():
-    # TODO: parse input & calculate the actual answer to the puzzle!
-
-    # Test
-    train = Training_data("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb".split())
-    train.decode()
-    print("Code so far:", train.code)
-    print("Cipher:", train.cipher)
+    (train, test) = parse_input()
+    decoded_displays = decode_displays(train, test)
+    decoded_displays_int = list(map(intlist_to_int, decoded_displays))
     
-##    foo = parse_input()
-##
-##    # Part 1
-##    print("=== Part 1 ===")
-##    answer = "?"
-##    print("Answer:", answer)
-##
-##    # Part 2
-##    print("=== Part 2 ===")
-##    answer = "?"
-##    print("Answer:", answer)
+    # Part 1
+    print("=== Part 1 ===")
+    counts_1478 = [count_1478(display) for display in decoded_displays]
+    answer = sum(counts_1478)
+    print("Answer:", answer)
+
+    # Part 2
+    print("=== Part 2 ===")
+    answer = sum(decoded_displays_int)
+    print("Answer:", answer)
 
 
-def parse_input() -> List[int]:
+def decode_displays(train: List[Training_data], test: List[Test_data]) -> List[List[int]]:
+    decoded_displays = []
+    for index in range(len(test)):
+        train[index].decode()
+        test[index].decode(train[index].code)
+        decoded_displays.append(test[index].decoded_digits)
+        
+    return decoded_displays
+
+
+def count_1478(display: List[int]) -> int:
+    return display.count(1) + display.count(4) + display.count(7) + display.count(8)
+        
+
+def intlist_to_int(intlist: List[int]) -> int:
+    return int("".join([str(digit) for digit in intlist]))
+
+
+def parse_input() -> (List[Training_data], List[Test_data]):
     input_file = open("day08_input.txt", "r")
     input_lines = input_file.read().strip().split("\n")
     input_file.close()
-    # do some other stuff...
 
-    return input_lines
+    train_list = []
+    test_list = []
+    for line in input_lines:
+        split_line = line.split(" | ")
+        train_list.append(split_line[0].split())
+        test_list.append(split_line[1].split())
+
+    train = [Training_data(line) for line in train_list]
+    test = [Test_data(line) for line in test_list]
+    return (train, test)
 
 
 if __name__ == "__main__":
