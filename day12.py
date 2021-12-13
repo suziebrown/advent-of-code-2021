f1323af58c11fbf0b4b3afe231b575ff38f8c1b7
Suzie Brown <suzie.brown42@gmail.com>
2021-12-13 22:25:22 +0000

    Day 12 Part 1 complete

--- day12.py
+++ day12.py
@@ -1,3 +1,5 @@
+import sys
+import threading
 from typing import List
 
 
@@ -8,28 +10,62 @@ class Cave:
         self.neighbours_names = []
 
     def is_small_cave(self) -> bool:
-        return self.name == self.name.upper()
+        return not self.name == self.name.upper()
 
     def add_neighbours(self, neighbours_names: List[str]):
         for name in neighbours_names:
             self.neighbours_names.append(name)
 
 
+def get_paths(start: Cave, end: Cave, caves: dict[str, Cave]) -> int:
+    global count
+    count = 0
+    get_paths_recursion(start, end, [], caves)
+    return count
+
+
+def get_paths_recursion(start: Cave, end: Cave, small_caves_visited: List[str], caves: dict[str, Cave]):
+    global count
+    print("Small caves visited so far:", small_caves_visited)
+    if start.is_small:
+        small_caves_visited.append(start.name)
+        
+    for name in start.neighbours_names:
+        neighbour = caves[name]
+        if neighbour.name in small_caves_visited:
+                continue
+
+        print("Arrived at node", name) 
+        if name == end.name:
+            count += 1
+            print("Count is now", count)
+        else:
+            get_paths_recursion(neighbour, end, small_caves_visited[::], caves)
+                
+            
 def main():
     input_lines = parse_input()
     caves = get_caves(input_lines)
     
     # Test
+##    for cave in caves.values():
+##        print(cave.name, "has neigbours", cave.neighbours_names)
+##    npaths = get_paths(caves["start"], caves["mj"], caves)
+##    print("Total paths found:", npaths)
     
     # Part 1
     print("=== Part 1 ===")
-    answer = "?"
+    answer = get_paths(caves["start"], caves["end"], caves)
     print("Answer:", answer)
 
     # Part 2
     print("=== Part 2 ===")
     answer = "?"
     print("Answer:", answer)
+
+
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
 
 
 def parse_input() -> List[List[str]]:
@@ -41,7 +77,7 @@ def parse_input() -> List[List[str]]:
     return pairs
 
 
-def get_caves(pairs: List[List[str]]) -> List[Cave]:
+def get_caves(pairs: List[List[str]]) -> dict[str, Cave]:
     caves = dict[str, Cave]()
     all_cave_names = unique(flatten(pairs))
     
